60283fa71f30f1f3c8ed4e2c222b2daaa18d846d
FAIZAH AHSAN <faizah1992@gmail.com>
2020-02-24 23:02:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-hopper== programmer_hash=[:grace_hopper][:known_for][:languages]
+hopper=programmer_hash=[:grace_hopper][:known_for][:languages]
 
 end
 
